0aa17ee5c514a56221a9e143efc9bf76e6e3ce5d
Aman Parida <amantechz467@gmail.com>
2026-06-06 19:30:07 +0530

    Navbar structure and basic things added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,120 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AnimatePresence, motion } from "motion/react";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+
+    const { userData } = useSelector((state) => state.user);
+    const credits = userData?.user?.credits;
+
+    const [showCredits, setShowCredits] = useState(false);
+    const [showProfile, setShowProfile] = useState(false);
+
     return (
-        <div>
-            Navbar
-        </div>
+        <motion.div
+            initial={{ opacity: 0, y: -15 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.50 }}
+            className='relative z-20 mx-6 mt-6 rounded-2xl bg-gradient-to-br from-black/90 via-black/80 to-black/90 backdrop-blur-2xl border border-white/10 shadow=[0_22px_55px_rgba(0,0,0,0.75)] flex items-center justify-between px-8 py-4'
+        >
+            {/* Left Side */}
+            <div
+                className='flex items-center gap-3'
+            >
+                <img src="/NeuroPrep-AI.png" alt="NeuroPrep_Logo" className='w-10 h-10' />
+                <span
+                    className='text-lg hidden md:block font-semibold text-white'
+                >
+                    NeuroPrep <span className='text-gray-400'>AI</span>
+                </span>
+            </div>
+
+            {/* Right Side */}
+            <div
+                className='flex items-center gap-6 relative'
+            >
+                <div className='relative'>
+                    <motion.div
+                        onClick={() => { setShowCredits(!showCredits); setShowProfile(false) }}
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0.95 }}
+                        className='flex items-center gap-1 px-4 py-2 rounded-full bg-white/10 border border-white/20 text-white-text-sm shadow-md cursor-pointer'
+                    >
+                        <span>💎</span>
+                        <span className='text-white'>{credits}</span>
+                        <motion.span
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.95 }}
+                            className='ml-2 h-5 w-5 flex items-center justify-center rounded-full bg-white text-xs font-bold'
+                        >
+                            ➕
+                        </motion.span>
+                    </motion.div>
+
+                    <motion.div
+
+                    >
+                        <AnimatePresence>
+                            {showCredits &&
+
+                                <motion.div
+                                    initial={{ opacity: 0, y: -10, scale: 0.95 }}
+                                    animate={{ opacity: 1, y: 10, scale: 1 }}
+                                    exit={{ opacity: 0, y: -10, scale: 0.95 }}
+                                    transition={{ duration: 0.4 }}
+                                    className='absolute right-[-50px] mt-4 w-64 rounded-2xl bg-black/90 backdrop-blur-xl border border-white/10 shadow-[0_25px_60px_rgba(0,0,0,0.7)] p-4 text-white'
+                                >
+                                    <h4 className='font-semibold mb-2'>Buy Credits</h4>
+                                    <p className='text-sm text-gray-300 mb-4'>
+                                        Use credit to generate AI Notes, diagrams & PDFs.
+                                    </p>
+                                    <button
+                                        onClick={() => setShowCredits(false)}
+                                        className='w-full py-2 rounded-lg bg-gradient-to-br from-white to-gray-200 text-black font-semibold hover:opacity-80 cursor-pointer'
+                                    >
+                                        Buy More Credits
+                                    </button>
+                                </motion.div>
+                            }
+                        </AnimatePresence>
+                    </motion.div>
+
+                </div>
+
+                <div className='relative'>
+                    <motion.div
+                        onClick={() => { setShowProfile(!showProfile); setShowCredits(false) }
+                        }
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0.95 }}
+                        className='flex items-center gap-1 px-4 py-2 rounded-full bg-white/10 border border-white/20 text-white-text-sm shadow-md cursor-pointer'
+                    >
+                        <span className='text-lg text-white'>{userData?.user?.name.slice(0, 1).toUpperCase()}</span>
+                    </motion.div>
+                    <AnimatePresence>
+                        {showProfile &&
+                            <motion.div
+                                initial={{ opacity: 0, y: -10, scale: 0.95 }}
+                                animate={{ opacity: 1, y: 10, scale: 1 }}
+                                exit={{ opacity: 0, y: -10, scale: 0.95 }}
+                                transition={{ duration: 0.4 }}
+                                className='absolute right-0 mt-4 w-52 rounded-2xl bg-black/90 backdrop-blur-xl border border-white/10 shadow-[0_25px_60px_rgba(0,0,0,0.7)] p-4 text-white'
+                            >
+
+                            </motion.div>
+                        }
+                    </AnimatePresence>
+
+                </div>
+
+            </div>
+        </motion.div >
+    )
+}
+
+function MenuItem({ onClick, text, red }) {
+    return (
+        
     )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className='min-h-screen overflow-hidden bg-white text-black'>
+            <Navbar />
         </div>
     )
 }
