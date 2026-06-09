086a72e5dc072fcee0b95b0eef195883c4798d3c
Aman Parida <amantechz467@gmail.com>
2026-06-09 21:01:34 +0530

    API for AI Notes fully created

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import userRouter from "./routes/user.route.js";
+import notesRouter from "./routes/generate.route.js";
 
 // .env config
 if (process.env.NODE_ENV !== "production") {
@@ -33,6 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 // API Routes
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
+app.use("/api/notes", notesRouter);
 
 
 // App Entry Point
